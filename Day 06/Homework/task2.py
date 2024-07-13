1e055e710b969b56fe7d575931be3fbba6264936
giorgigagoshidze <g18900437@gmail.com>
2024-07-13 08:16:19 -0700

    fourth commit

--- Day 06/Homework/task2.py
+++ Day 06/Homework/task2.py
@@ -1,6 +1,5 @@
 #2. შექმენით სამი ცვლადი, სამივეში შეინახეთ განსხვავებული ინფორმაციის ტიპები, შემდეგ კი დაპრინტეთ ისინი
-age = (12)
-name = ("Luka")
-surname = ("tsertsvadze")
-print(age)
-print(name +" "+ surname)
+age = 12
+name = "Luka"
+surname = "tsertsvadze"
+print(str(age)+" "+ name +" "+ surname) 
--- Day 06/Homework/task3.py
+++ Day 06/Homework/task3.py
@@ -2,4 +2,4 @@
 #სადაც მომხმარებელს შემოატანინებთ გვარს, ბოლოს დაუპრინტეთ მსგავსი მესიჯი: 'hello ' + (სახელი) + (გვარი).
 user_name = input()
 last_name = input()
-print("welcome " + (user_name) +" "+ (last_name) )
+print("welcome " + user_name +" "+ last_name )
